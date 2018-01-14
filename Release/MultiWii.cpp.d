493820ce8ee176c7f9d303241c4b18e6f1fda2fe
prismdata <prismdata@naver.com>
2018-01-14 22:04:32 +0900

    Remove predefined parameter(#define block)

--- Release/MultiWii.cpp.d
+++ Release/MultiWii.cpp.d
@@ -11,7 +11,7 @@ MultiWii.cpp.o: ../MultiWii.cpp \
  /Applications/EclipseCPP.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h \
  /Applications/EclipseCPP.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/variants/standard/pins_arduino.h \
  ../config.h ../def.h ../types.h ../MultiWii.h ../Alarms.h ../EEPROM.h \
- ../IMU.h ../LCD.h ../Output.h ../RX.h ../Sensors.h ../Serial.h ../GPS.h \
+ ../IMU.h ../Output.h ../RX.h ../Sensors.h ../Serial.h ../GPS.h \
  ../Protocol.h \
  /Applications/EclipseCPP.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/libraries/Wire/src/Wire.h \
  /Applications/EclipseCPP.app/Contents/Eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Stream.h
@@ -51,8 +51,6 @@ MultiWii.cpp.o: ../MultiWii.cpp \
 ../EEPROM.h:
 
 ../IMU.h:
-
-../LCD.h:
 
 ../Output.h:
 
